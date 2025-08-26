899a199e52a04ea6ee4fe77c0442c45dbd1b0e5a
Anurag Malik <anuragmalik2007@gmail.com>
2025-08-26 23:15:06 +0530

    Add files via upload

---
+++ perimeter of rectangle.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  perimeter of rectangle
+//
+//  Created by Anurag Malik on 26/08/25.
+//
+
+
+
+
+
+#include <stdio.h>
+
+int main(void){
+    int l,b,p;
+    printf("enter length and breadth");
+    scanf("%d,%d",&l,&b);
+    
+    printf(" perimeter of rectangle is %d", p=l+b);
+    return 0;
+}
